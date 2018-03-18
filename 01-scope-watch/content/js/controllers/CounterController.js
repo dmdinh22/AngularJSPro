95bc2cf80ff78592b2d467b862bcfc33c1ef83e6
David Dinh <dmdinh22@gmail.com>
2018-03-18 16:08:30 -0700

    added comments for future ref

--- 01-scope-watch/content/js/controllers/CounterController.js
+++ 01-scope-watch/content/js/controllers/CounterController.js
@@ -1,14 +1,27 @@
 function CounterController($scope) {
+    // $scope.count = 0;
     this.count = 0;
     this.countList = [];
     this.increment = function increment() {
+        // $scope.count++;
         this.count++;
         this.countList.unshift({id: this.count});
     };
     this.decrement = function decrement() {
+        // $scope.count--;
         this.count--;
         this.countList.unshift({id: this.count});
     };
+    // $scope.$watch(angular.bind(this, function() {
+    //     //return this.count;
+    //     return this.countList;
+    // }), function(newValue, oldValue) {
+    //     if (newValue === oldValue) {
+    //         return;
+    //     }
+    //     console.log(newValue,oldValue);
+    // }, true); // third arg 'true' for deepWatch
+    // watchCollection - good for observing arrays of objects
     $scope.$watchCollection(angular.bind(this, function () {
         return this.countList;
     }), function (newValue, oldValue) {
