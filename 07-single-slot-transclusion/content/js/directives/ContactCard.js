24fd486d13f5e451bd43f55413089c1c7ef1f6ef
David Dinh <dmdinh22@gmail.com>
2018-03-21 00:13:33 -0700

    single slot transclusion

--- 07-single-slot-transclusion/content/js/directives/ContactCard.js
+++ 07-single-slot-transclusion/content/js/directives/ContactCard.js
@@ -1,15 +1,15 @@
 function ContactCard() {
-	return {
-		transclude: true,
-		template: `
+    return {
+        transclude: true, // templating things inside a template
+        template: `
 			<div>
 				<h4>Contact</h4>
 				<div ng-transclude></div>
 			</div>
 		`
-	}
+    };
 }
 
 angular
-	.module('app')
-	.directive('contactCard', ContactCard);
+    .module('app')
+    .directive('contactCard', ContactCard);
