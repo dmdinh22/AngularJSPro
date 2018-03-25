5aa95752dd5203b53c9889290ecdff90852a6948
David Dinh <dmdinh22@gmail.com>
2018-03-25 02:48:51 -0700

    lifecycle hooks

--- 14-require-init-postlink-destroy/content/js/components/tabs.js
+++ 14-require-init-postlink-destroy/content/js/components/tabs.js
@@ -1,24 +1,24 @@
 var tabs = {
-	transclude: true,
-	controller: function () {
-		this.tabs = [];
-		this.addTab = function addTab(tab) {
-			this.tabs.push(tab);
-		};
-		this.selectTab = function selectTab(index) {
-			for (var i = 0; i < this.tabs.length; i++) {
-				this.tabs[i].selected = false;
-			}
-			this.tabs[index].selected = true;
-		};
-		this.$postLink = function () {
-			this.selectTab(0);
-		};
-		this.$onDestroy = function () {
+    transclude: true,
+    controller: function () {
+        this.tabs = [];
+        this.addTab = function addTab(tab) {
+            this.tabs.push(tab);
+        };
+        this.selectTab = function selectTab(index) {
+            for (var i = 0; i < this.tabs.length; i++) {
+                this.tabs[i].selected = false;
+            }
+            this.tabs[index].selected = true;
+        };
+        this.$postLink = function () {
+            this.selectTab(0);
+        };
+        this.$onDestroy = function () {
 
-		};
-	},
-	template: `
+        };
+    },
+    template: `
 		<div class="tabs">
 			<ul class="tabs__list">
 				<li ng-repeat="tab in $ctrl.tabs">
@@ -33,5 +33,5 @@ var tabs = {
 };
 
 angular
-	.module('app')
-	.component('tabs', tabs);
+    .module('app')
+    .component('tabs', tabs);
