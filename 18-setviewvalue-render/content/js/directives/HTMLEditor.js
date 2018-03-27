61d5fca84c99db0c73d704d3676709cb29c37919
David Dinh <dmdinh22@gmail.com>
2018-03-26 23:15:11 -0700

    render, setViewValue

--- 18-setviewvalue-render/content/js/directives/HTMLEditor.js
+++ 18-setviewvalue-render/content/js/directives/HTMLEditor.js
@@ -1,21 +1,23 @@
 function HTMLEditor() {
-	return {
-		require: 'ngModel',
-		link: function ($scope, $element, $attrs, $ctrl) {
-			var ngModelCtrl = $ctrl;
-			// View -> Model
-			$element.on('input', function (event) {
-				ngModelCtrl.$setViewValue(event.target.innerHTML);
-			});
-			// Model -> View
-			ngModelCtrl.$render = function () {
-				$element.html(ngModelCtrl.$modelValue);
-			};
-			ngModelCtrl.$setViewValue($element[0].innerHTML);
-		}
-	};
+    return {
+        // because ng-model is bound to markup
+        require: 'ngModel', 
+        link: function ($scope, $element, $attrs, $ctrl) {
+            var ngModelCtrl = $ctrl;
+            // View -> Model
+            $element.on('input', function (event) {
+                ngModelCtrl.$setViewValue(event.target.innerHTML);
+            });
+            // Model -> View
+            ngModelCtrl.$render = function () {
+                $element.html(ngModelCtrl.$modelValue);
+            };
+            //				          same as jQuery's element.html()
+            ngModelCtrl.$setViewValue($element[0].innerHTML);
+        }
+    };
 }
 
 angular
-	.module('app')
-	.directive('htmlEditor', HTMLEditor);
+    .module('app')
+    .directive('htmlEditor', HTMLEditor);
