3aaf5efcfc16c5f0cc3bcdd7b236be9c28b7361c
David Dinh <dmdinh22@gmail.com>
2018-03-27 00:59:05 -0700

    validators pipeline

--- 19-parsers-formatters/content/js/controllers/CheckoutController.js
+++ 19-parsers-formatters/content/js/controllers/CheckoutController.js
@@ -1,9 +1,11 @@
 function CheckoutController() {
+    // default object for form
     this.details = {
         username: '',
         password: '',
         coupon: 'summer-50'
     };
+    // submit event
     this.onSubmit = function () {
         console.log(this.details);
     };
